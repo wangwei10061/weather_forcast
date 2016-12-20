ae313deec8de1ea975a5a4ae28ea098689a260fe
wangewei10061 <153692771@qq.com>
2016-12-20 10:58:05 +0800

    删除部分注释
    删除部分注释

--- app.js
+++ app.js
@@ -9,9 +9,3 @@ App({
   globalData: {
   }
 })
-/**
- * 
- * 从百度API获取城市，根据当前经纬度
- * 
- * http://api.map.baidu.com/geocoder/v2/?ak=pmCgmADsAsD9rEXkqWNcTzjd&location=30.548397,104.04701&output=json&pois=1
- */
